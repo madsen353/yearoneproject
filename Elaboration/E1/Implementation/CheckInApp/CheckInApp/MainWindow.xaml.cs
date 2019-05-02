8072cb26ca08ab379560a96c101f4b8a85a0ded2
Rasmus <rasm3600@student.eadania.dk>
2019-05-02 13:08:50 +0200

    Moved button actions from Ebys solution to the merged solution.

--- Elaboration/E1/Implementation/CheckInApp/CheckInApp/MainWindow.xaml.cs
+++ Elaboration/E1/Implementation/CheckInApp/CheckInApp/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using Renci.SshNet;
 using CheckInApp.ServerConfiguration;
 using Newtonsoft.Json;
+using System.IO;
+
 
 namespace CheckInApp
 {
@@ -30,6 +32,8 @@ namespace CheckInApp
 
         private void ConnectToServerBTN_Click(object sender, RoutedEventArgs e)
         {
+
+            //Made by Rasmus
             RemoteServerConfig config = ConfigurationReader.Read();
             RemoteServer server = new RemoteServer(config);
             List<RawBooking> rawBookings = new List<RawBooking>();
@@ -39,6 +43,46 @@ namespace CheckInApp
             }
             BookingMapper bookingmapper = new BookingMapper();
             List<Booking> bookings = bookingmapper.Map(rawBookings);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Made by Eby
+            using (var sftp = new SftpClient("10.152.120.24", "funhall", "ubuntu123"))
+            {
+                sftp.Connect();
+                MessageBox.Show("connected");
+                var files = sftp.ListDirectory(@"bookings/");//SFTP folder from where the file is to be download
+
+                //Save all files in remoteserver to the local folder FlexyBox
+
+                foreach (var file in files)
+                {
+                    if (file.Name.EndsWith(".json"))
+                    {
+                        string remoteFileName = file.Name;
+
+                        using (Stream file1 = File.Create(@"FlexyBox/" + remoteFileName))
+                        {
+                            sftp.DownloadFile(@"bookings/" + remoteFileName, file1);
+                        }
+                    }
+                }
+                sftp.Disconnect();
+            }
+
+            //Get all files in the folder 'FlexyBox'
+            string[] Files = Directory.GetFiles(@"FlexyBox/", "*.json");
+
+            //Deserialize each file and save data to DB
+            foreach (string file in Files)
+            {
+
+                Booking booking = JsonConvert.DeserializeObject<Booking>(File.ReadAllText(file),
+                    new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+                DAL dalObject = new DAL();
+                dalObject.InsertBookingToDb(); //Save data to Db
+            }
         }
     }
 }
