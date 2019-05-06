9f74de9d0e479a2c8350d1cbb9194fa91476370e
Rasmus <rasm3600@student.eadania.dk>
2019-05-06 11:55:41 +0200

    Merged Eby and Rasmus project into Anders and Niels

---
+++ Funhall2/ServerConfiguration/ConfigurationReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace Funhall2
+{
+    //Made by Rasmus
+
+    public static class ConfigurationReader
+    {
+        public static RemoteServerConfig Read()
+        {
+            return new RemoteServerConfig
+            {
+                Username = ConfigurationManager.AppSettings["username"],
+                Password = ConfigurationManager.AppSettings["password"],
+                RemoteAddress = ConfigurationManager.AppSettings["remoteAddress"],
+                FilePath = ConfigurationManager.AppSettings["filePath"]
+            };
+        }
+    }
+}
---
+++ Funhall2/ServerConfiguration/RemoteServerConfig.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Funhall2
+{
+    //Made by Rasmus
+
+    public class RemoteServerConfig
+    {
+        public string RemoteAddress { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FilePath { get; set; }
+    }
+}
