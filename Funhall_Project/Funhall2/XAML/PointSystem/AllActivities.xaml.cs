1e2ddcfbfdf4e0cdfd7121848ccee41c89c5e9b6
Rasmus <rasm3600@student.eadania.dk>
2019-05-10 12:24:30 +0200

    Ive come so far that im ready to initialise the customers in the point system, but i need new database query from Eby

--- Funhall_Project/Funhall2/XAML/PointSystem/AllActivities.xaml.cs
+++ Funhall_Project/Funhall2/XAML/PointSystem/AllActivities.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Funhall2.Classes;
 
 namespace Funhall2.XAML.PointSystem
 {
@@ -20,9 +22,23 @@ namespace Funhall2.XAML.PointSystem
     /// </summary>
     public partial class AllActivities : Page
     {
+        public Booking Booking { get; set; }
         public AllActivities(Booking booking)
         {
+            Booking = booking;
             InitializeComponent();
+            this.DataContext = Booking;
+
+            ObservableCollection<Activity> activities = Activity.getBookedActivities(Booking);
+            Activities.ItemsSource = activities;
+
+        }
+
+        private void ShowSelectedActivity(object sender, RoutedEventArgs e)
+        {
+            Activity a = Activities.SelectedItem as Activity;
+            CurrentActivity allActivities = new CurrentActivity(a, Booking);
+            this.NavigationService.Navigate(allActivities);
         }
     }
 }
---
+++ Funhall_Project/Funhall2/XAML/PointSystem/CurrentActivity.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Funhall2.Classes;
+
+namespace Funhall2.XAML.PointSystem
+{
+    /// <summary>
+    /// Interaction logic for CurrentActivity.xaml
+    /// </summary>
+    public partial class CurrentActivity : Page
+    {
+        public CurrentActivity(Activity a, Booking b)
+        {
+            InitializeComponent();
+            ObservableCollection<Customer> guests = Customer.GetCustomers(b);
+            Guests.ItemsSource = guests;
+        }
+
+        private void UpdateScore(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
