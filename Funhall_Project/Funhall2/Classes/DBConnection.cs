899c567018790190e321f1626d02e3b713bc7abd
eby <ebytreesa@gmail.com>
2019-05-16 19:14:30 +0200

    db conn class created

---
+++ Funhall_Project/Funhall2/Classes/DBConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace Funhall2.Classes
+{
+    public class DBConnection
+    {
+        public SqlConnection con;
+
+        public DBConnection()
+        {
+             con = new SqlConnection("Data Source=.;Initial Catalog=FunHall;"
+                                + "Integrated Security=true;");
+             con.Open();
+        }
+
+        public void ConnectionClose()
+        {
+            con.Close();
+        }
+    }
+}
