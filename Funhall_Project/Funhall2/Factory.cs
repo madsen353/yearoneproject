4fa4158220b5bd2dea175256bf064590b868fc5e
eby <ebytreesa@gmail.com>
2019-05-24 21:32:36 +0200

    new factory and interfaces for DIP

---
+++ Funhall_Project/Funhall2/Factory.cs
@@ -0,0 +1,28 @@
+﻿using Funhall2.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funhall2
+{
+    public static class Factory
+    {
+        public static IDAL CreateDAL()
+        {
+            return new DAL();
+        }
+
+        public static IActivity CreateActivity()
+        {
+            return new Activity();
+        }
+
+        public static ICustomer CreateCustomer()
+        {
+            return new Customer();
+        }
+
+    }
+}
---
+++ Funhall_Project/Funhall2/Interfaces/IDAL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+
+namespace Funhall2.Classes
+{
+    public interface IDAL
+    {
+        void AddActivities(ICustomer cus);
+        //void CheckInCus(Customer cus);
+        void EndActivity(string id, bool isFinished, string activityName);
+        ObservableCollection<Activity> GetBookedActivities(Booking booking);
+        ObservableCollection<Booking> getBookings();
+        ObservableCollection<CustomerActivity> GetCusActivities(Customer cus);
+        CustomerActivity GetCusActivitySpecifiedByActivity(Customer cus, Activity act);
+        ObservableCollection<Customer> GetCustomers(Booking booking);
+        List<int> GetTotalAmountOfPoints(int guestID);
+        void InsertActivityToDb(Booking booking);
+        void InsertBookedActivitiesToDb(Booking booking);
+        void InsertBookedProductsToDb(Booking booking);
+        void InsertBookingToDb(Booking booking);
+        void OpenConnection(SqlConnection con);
+        void ResetDAL();
+        void UpdateCus(Customer cus);
+        void UpdateCusActivity(int id, string points, string activityName);
+        void CheckInCus(ICustomer cus);
+    }
+}
